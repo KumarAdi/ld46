79a9e400678dc7a66613e8d407ba36646323174a
Patrick Mao <pmao@uw.edu>
2020-04-20 12:54:36 -0700

    tutorial polishing

--- src/scenes/FirstLevel.hx
+++ src/scenes/FirstLevel.hx
@@ -22,20 +22,25 @@ class FirstLevel extends MapSelectLevel {
         tutorialIdx = 0;
 
         var tutorials = [
-            {text: "The caravan is you", loc: curTown.point},
-            {text: "The black lines are roads", loc: curTown.point},
+            {text: "This is your caravan", loc: curTown.point},
+            {text: "The brown lines are roads", loc: curTown.point},
             {text: "You can travel to neighboring towns by clicking on them", loc: curTown.getNeighbors()[0]},
-            {text: "Your goal is to get to this town", loc: endTown.point}
+            {text: "The Great Wizard lives in this town!", loc: endTown.point}
         ];
 
+        // init tutorial box
         var tutorialParent = new Object(scene);
-        var tutorialTile = Tile.fromColor(0x000000, 400, 100);
+        var tutorialTile = Tile.fromColor(0x000000);
+        tutorialTile.scaleToSize(380, 300);
         var tutorialBg = new Bitmap(tutorialTile, tutorialParent);
-        tutorialBg.alpha = 0.5;
-        var tutorialBox = new Interactive(400, 200, tutorialParent);
+        tutorialBg.alpha = 0.7;
+        var tutorialBox = new Interactive(tutorialBg.getBounds().width, tutorialBg.getBounds().height, tutorialParent);
         var tutorialText = new Text(Res.fonts.centurygothic.toFont(), tutorialParent);
-        tutorialText.maxWidth = tutorialTile.width;
+        tutorialText.maxWidth = tutorialBg.getBounds().width - 12;
+        tutorialText.x = 13;
+        tutorialText.y = 9;
 
+        // set tutorial box pos
         tutorialParent.x = curTown.point.x - (townTile.width / 2);
         tutorialParent.y = curTown.point.y - townTile.height/2 - 100;
 
@@ -48,17 +53,20 @@ class FirstLevel extends MapSelectLevel {
             }
 
             var tutorial = tutorials[tutorialIdx];
-            updateTutorial(tutorialParent, tutorialText, tutorialBg, tutorial);
+            updateTutorial(tutorialParent, tutorialText, tutorialBg, tutorial, tutorialTile, tutorialBox);
         }
 
         var tutorial = tutorials[tutorialIdx];
-        updateTutorial(tutorialParent, tutorialText, tutorialBg, tutorial);
+        updateTutorial(tutorialParent, tutorialText, tutorialBg, tutorial, tutorialTile, tutorialBox);
     }
 
-    private function updateTutorial(tutorialParent: Object, tutorialText: Text, tutorialBg: Bitmap ,tutorial: {text: String, loc: Point}): Void {
-        tutorialText.text = tutorial.text + "\n\n <Click here to continue>";
-        tutorialBg.tile.scaleToSize(tutorialText.maxWidth, tutorialText.textHeight);
-        tutorialParent.x = Math.min(scene.width - tutorialText.maxWidth, tutorial.loc.x - (townTile.width / 2));
-        tutorialParent.y = Math.max(0, tutorial.loc.y - (townTile.height / 2 + tutorialText.textHeight));
+    private function updateTutorial(tutorialParent: Object, tutorialText: Text, tutorialBg: Bitmap ,tutorial: {text: String, loc: Point}, tutorialTile: Tile, tutorialBox: Interactive): Void {
+        // update tutorial box text, size, and pos
+        tutorialText.text = tutorial.text + "\n\n   -Click here to continue-";
+        tutorialTile.scaleToSize(380, tutorialText.textHeight + 20);
+        tutorialBox.width = tutorialBg.getBounds().width;
+        tutorialBox.height = tutorialBg.getBounds().height;
+        tutorialParent.x = Math.min(scene.width - tutorialText.maxWidth - 20, tutorial.loc.x - (townTile.width / 2));
+        tutorialParent.y = Math.max(20, tutorial.loc.y - (townTile.height / 2 + tutorialText.textHeight));
     }
 }
