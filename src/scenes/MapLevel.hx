9692fa9baf50190aab4ce48173b906fea7db470c
Patrick Mao <pmao@uw.edu>
2020-04-20 03:12:43 -0700

    ui almost done, need to add relics

--- src/scenes/MapLevel.hx
+++ src/scenes/MapLevel.hx
@@ -1,5 +1,6 @@
 package scenes;
 
+import h2d.Slider;
 import h2d.col.Voronoi.Diagram;
 import h2d.Tile;
 import h2d.col.Voronoi.Cell;
@@ -31,6 +32,9 @@ class MapLevel implements Level {
     private var endTown: Cell;
     private var roads: Object;
     private var curse: Text;
+    private var curseBar: Bitmap;
+    private var curseBarFillTile: Tile;
+    private var curseIcon: Bitmap;
     private var supplies: Text;
     private var gold: Text;
 
@@ -85,6 +89,11 @@ class MapLevel implements Level {
         var caravanTile = Res.img.caravan.toTile();
         caravanTile.scaleToSize(townSize.x * 0.9, townSize.y * 0.9);
         playerMarker = new Bitmap(caravanTile, scene);
+
+        // UI STUFF
+
+        // Init font
+        var uiFont = Res.fonts.centurygothic.toFont();
         
         // var uiBgTile = Tile.fromColor(0, 1920, 200);
         // var uiBg = new Bitmap(uiBgTile, scene);
@@ -92,25 +101,38 @@ class MapLevel implements Level {
         uiBg.y = scene.height - 150;
         scene.addChild(uiBg);
 
-        var x = new Text(Res.fonts.alagard.toFont(), uiBg);
-        x.text = "X";
-        x.scale(3);
-
+        // Init back button
+        var x = new Text(uiFont, scene);
+        x.text = "Back to Dialogue";
         var xBtn = new Interactive(x.textHeight, x.textHeight, x);
-
         xBtn.onClick = function (e: Event) {
             nextLevel = parent;
         }
 
+        // Init curse bar
         var curseBarTile = Res.img.bar.toTile();
         curseBarTile.scaleToSize(480, 120);
-        var curseBar = new Bitmap(curseBarTile, uiBg);
-        curseBar.x = 100;
+        curseBar = new Bitmap(curseBarTile, uiBg);
+        curseBar.x = 150;
+        trace(playerData.flags.get("curse"));
+
+        curseBarFillTile = Tile.fromColor(0x3c2a59);
+        curseBarFillTile.scaleToSize(40, 33);
+        var curseBarFill = new Bitmap(curseBarFillTile, curseBar);
+        curseBarFill.x = 46;
+        curseBarFill.y = 43;
         var curseTile = Res.img.curse.toTile();
         curseTile.scaleToSize(120, 120);
-        var curseIcon = new Bitmap(curseTile, curseBar);
-        curseIcon.x = 20;
+        curseIcon = new Bitmap(curseTile, curseBar);
+        curseIcon.x = 360;
 
+        // Init curse text
+        curse = new Text(uiFont, curseBar);
+        curse.text = "The Curse";
+        curse.x = (curseBar.getBounds().width / 2) - (curse.textWidth / 2) - 5;
+        curse.y = curseBar.getBounds().height - 24;
+
+        // Init relics
         var crystalMissingTile = Res.img.crystalmissing.toTile();
         crystalMissingTile.scaleToSize(120,120);
         var crystalMissingIcon = new Bitmap(crystalMissingTile, uiBg);
@@ -119,19 +141,36 @@ class MapLevel implements Level {
         var orbMissingTile = Res.img.orbmissing.toTile();
         orbMissingTile.scaleToSize(120,120);
         var orbMissingIcon = new Bitmap(orbMissingTile, uiBg);
-        orbMissingIcon.x = crystalMissingIcon.x + crystalMissingIcon.getBounds().width + 100;
+        orbMissingIcon.x = crystalMissingIcon.x + crystalMissingIcon.getBounds().width + 50;
 
         var stoneMissingTile = Res.img.stonemissing.toTile();
         stoneMissingTile.scaleToSize(120,120);
         var stoneMissingIcon = new Bitmap(stoneMissingTile, uiBg);
-        stoneMissingIcon.x = orbMissingIcon.x + orbMissingIcon.getBounds().width + 100;
+        stoneMissingIcon.x = orbMissingIcon.x + orbMissingIcon.getBounds().width + 50;
 
-        curse = new Text(Res.fonts.alagard.toFont(), uiBg);
-        curse.x = 400;
-        supplies = new Text(Res.fonts.alagard.toFont(), uiBg);
-        supplies.x = 800;
-        gold = new Text(Res.fonts.alagard.toFont(), uiBg);
-        gold.x = 1200;
+        // Init relics text
+        var relics = new Text(uiFont, uiBg);
+        relics.text = "The Relics";
+        relics.x = orbMissingIcon.x + (orbMissingIcon.getBounds().width / 2) - (relics.textWidth / 2) - 5;
+        relics.y = orbMissingIcon.getBounds().height - 24;
+        
+        // Init supplies text
+        var suppliesTitle = new Text(uiFont, uiBg);
+        suppliesTitle.text = "Supplies";
+        supplies = new Text(Res.fonts.alagard.toFont(), suppliesTitle);
+        supplies.x = 50;
+        supplies.y = 40;
+        suppliesTitle.x = scene.width - 350;       
+        suppliesTitle.y = 30; 
+
+        // Init money text
+        var goldTitle = new Text(uiFont, uiBg);
+        goldTitle.text = "Satchel";
+        gold = new Text(Res.fonts.alagard.toFont(), goldTitle);
+        gold.x = 50;
+        gold.y = 40;
+        goldTitle.x = suppliesTitle.x - 250;
+        goldTitle.y = 30;
     }
 
     public function init() {
@@ -151,9 +190,12 @@ class MapLevel implements Level {
         playerMarker.x = curTown.point.x - (townTile.width * 0.45);
         playerMarker.y = curTown.point.y - (townTile.height * 0.45);
 
-        curse.text = "Curse: " + playerData.flags.get("curse");
-        supplies.text = "Supplies: " + playerData.flags.get("supplies");
-        gold.text = "Gold: " + playerData.flags.get("money");
+        // var cursePercent = playerData.flags.get("curse") / 10;
+        curseBarFillTile.scaleToSize((40 * playerData.flags.get("curse")), 33);
+        //curseIcon.x = curseBarFill.getBounds().width - 50;
+        
+        supplies.text = playerData.flags.get("supplies") + " days";
+        gold.text = playerData.flags.get("money") + " g";
 
         // null out nextLevel so it's not set when we come back
         var ret = nextLevel;
